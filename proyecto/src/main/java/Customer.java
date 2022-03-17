d8c5920ab58128b67f12c5cf78a00693568669c0
hermes1 <hcarlop505@g.educaand.es>
2022-03-17 18:26:18 +0100

    Commit 1

--- proyecto/src/main/java/Customer.java
+++ proyecto/src/main/java/Customer.java
@@ -23,21 +23,7 @@ public class Customer {
             Rental each = (Rental) rentals.nextElement();
 
             // determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
+            amountFor(thisAmount, each);
 
             // add frequent renter points
             frequentRenterPoints ++;
@@ -56,5 +42,26 @@ public class Customer {
 
     public String getName() {
         return _name;
+    }
+    
+    public double amountFor(double thisAmount, Rental each){
+        switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2)
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3)
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    break;
+            }
+        
+        return thisAmount;
+        
     }
 }
