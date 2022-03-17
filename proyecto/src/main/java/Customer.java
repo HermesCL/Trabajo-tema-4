8f0e206584575cb5c7d8dc56eae3129e8e07ec8a
hermes1 <hcarlop505@g.educaand.es>
2022-03-17 19:39:21 +0100

    Commit 3

--- proyecto/src/main/java/Customer.java
+++ proyecto/src/main/java/Customer.java
@@ -1,20 +1,24 @@
+
 import java.util.Enumeration;
 import java.util.Vector;
 
 public class Customer {
+
     private String _name;
     private Vector _rentals = new Vector();
 
-    public Customer (String name) {
+    public Customer(String name) {
         _name = name;
     }
 
-    public void addRental (Rental arg) {
+    public void addRental(Rental arg) {
         _rentals.addElement(arg);
     }
 
-    public String statement () {
+    public String statement() {
         double totalAmount = 0;
+        //Ya que hemos definido la variable dentro del metodo getFrecuentRenterPoint podemos eliminarla
+        // int frequentRenterPoints = 0;
         int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
@@ -27,10 +31,12 @@ public class Customer {
             rental1.getGetCharge(thisAmount, each);
 
             // add frequent renter points
-            frequentRenterPoints ++;
+            //Ya que hemos sumado dentro del metodo podemos eliminar esta parte
+            //frequentRenterPoints++;
+            
             // add bounus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints ++;
+            getFrecuentRenterPoint(each);
+            
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
@@ -44,6 +50,17 @@ public class Customer {
     public String getName() {
         return _name;
     }
-    
-   
+
+    public int getFrecuentRenterPoint(Rental each) {
+        int frequentRenterPoints = 1;
+        
+        //El if comprueba que la pelicula sea nueva y se haya alquilado mas de un dia
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
+            //si se cumple se le añadira 1 a la variable frequentRenterPoint con lo cual se retornara 2
+            //en caso contrario se retornara 1 que es el valor de inicializacion de la variable
+            frequentRenterPoints++;
+        }
+        return frequentRenterPoints;
+    }
+
 }
